67ad6bf034a07b3dbb73ffef3f09200aadfed826
Huzaifa <Huzaifa199872@hotmail.com>
2022-02-16 22:37:29 +0500

    Added Favicon and added Bootstrap 5 Style adminpannel and login page

---
+++ ARS/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ARS.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login() 
+        { 
+            return View(); 
+        }
+        
+    }
+}
