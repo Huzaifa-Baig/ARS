0b04f0e0fccf1efb623cf12cca07fad889dc433c
Huzaifa <Huzaifa199872@hotmail.com>
2022-02-16 23:05:38 +0500

    ameded some files

--- ARS/Controllers/AdminController.cs
+++ ARS/Controllers/AdminController.cs
@@ -3,20 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ARS.Models;
 
 namespace ARS.Controllers
 {
     public class AdminController : Controller
     {
+        Context cs = new Context();
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
+        //GET: Login
         public ActionResult login() 
         { 
             return View(); 
         }
-        
+        //POST: Login
+        [HttpPost]
+        public ActionResult login(AdminLogin l)
+        {
+            var x = cs.AdminLogin.Where(a=> a.AdminName == l.AdminName && a.AdminPassword == l.AdminPassword).ToList();
+            if(x != null)
+            {
+                return RedirectToAction("dashboard");
+            }
+            else
+            {
+                ViewBag.message="Wrong Id or Password";
+            }
+            return View();
+        }
+
+        //GET: Dashboard
+        public ActionResult dashboard()
+        {
+            return View();
+        }
     }
 }
